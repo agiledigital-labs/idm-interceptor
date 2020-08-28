1872bc4dbee0fd4411cee43c23998d7c8598b66d
Joel Pearson <joel.pearson@gmail.com>
2020-08-28 21:56:30 +1000

    ADE-415 Get interception working properly

--- src/main/java/au/com/agiledigital/idm/RequestHandlerInterceptor.java
+++ src/main/java/au/com/agiledigital/idm/RequestHandlerInterceptor.java
@@ -1,9 +1,11 @@
 package au.com.agiledigital.idm;
 
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Set;
 
 import org.apache.felix.dm.annotation.api.AspectService;
+import org.apache.felix.dm.annotation.api.ServiceDependency;
+import org.forgerock.json.JsonValue;
 import org.forgerock.json.resource.ActionRequest;
 import org.forgerock.json.resource.ActionResponse;
 import org.forgerock.json.resource.CreateRequest;
@@ -15,29 +17,48 @@ import org.forgerock.json.resource.QueryResponse;
 import org.forgerock.json.resource.ReadRequest;
 import org.forgerock.json.resource.RequestHandler;
 import org.forgerock.json.resource.ResourceException;
+import org.forgerock.json.resource.ResourcePath;
 import org.forgerock.json.resource.ResourceResponse;
 import org.forgerock.json.resource.UpdateRequest;
+import org.forgerock.openidm.config.enhanced.EnhancedConfig;
+import org.forgerock.openidm.crypto.CryptoService;
+import org.forgerock.openidm.provisioner.openicf.commons.ConnectorUtil;
+import org.forgerock.openidm.provisioner.openicf.commons.ObjectClassInfoHelper;
 import org.forgerock.services.context.Context;
 import org.forgerock.util.promise.Promise;
+import org.identityconnectors.framework.common.objects.Attribute;
 import org.osgi.framework.ServiceReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @AspectService(ranking = 10, added = "added", changed = "changed", removed = "removed", filter = "(&(objectClass=org.forgerock.json.resource.RequestHandler)(service.factoryPid=org.forgerock.openidm.provisioner.openicf))")
 public class RequestHandlerInterceptor implements RequestHandler {
-	// The service we are intercepting (injected by reflection)
 	private volatile RequestHandler intercepted;
-	private String factoryPid;
+	private volatile String factoryPid;
+	@ServiceDependency
+	private volatile EnhancedConfig enhancedConfig;
+	
+	@ServiceDependency
+	private CryptoService cryptoService;
+	
+	private Map<String, ObjectClassInfoHelper> objectClassInfoMap;
 
 	private static Logger logger = LoggerFactory.getLogger(RequestHandlerInterceptor.class);
-
-	private Map<String, RequestHandler> nameConnectors = new ConcurrentHashMap<>();
-	private Map<RequestHandler, String> handlerConnectors = new ConcurrentHashMap<>();
+	
+	private Map<String, ObjectClassInfoHelper> generateObjectClassInfo(ServiceReference<?> ref) {
+		JsonValue jsonConfig = this.enhancedConfig.getConfigurationAsJson(new ComponentContextAdapter(ref));
+		Map<String, ObjectClassInfoHelper> objectTypes = ConnectorUtil.getObjectTypes(jsonConfig);
+		
+		return objectTypes;
+	}
+	
+	private ObjectClassInfoHelper getObjectClassInfoHelper(ResourcePath resourcePath) {
+		return objectClassInfoMap.get(resourcePath.get(0));
+	}
 
 	public void added(ServiceReference<RequestHandler> ref, RequestHandler service) {
 		String factoryPid = (String) ref.getProperty("config.factory-pid");
-		nameConnectors.put(factoryPid, service);
-		handlerConnectors.put(service, factoryPid);
+		this.objectClassInfoMap = generateObjectClassInfo(ref);
 		this.factoryPid = factoryPid;
 		this.intercepted = service;
 
@@ -46,10 +67,7 @@ public class RequestHandlerInterceptor implements RequestHandler {
 
 	public void changed(ServiceReference<RequestHandler> ref, RequestHandler service) {
 		String factoryPid = (String) ref.getProperty("config.factory-pid");
-		RequestHandler originalHandler = nameConnectors.remove(factoryPid);
-		handlerConnectors.remove(originalHandler);
-		nameConnectors.put(factoryPid, service);
-		handlerConnectors.put(service, factoryPid);
+		this.objectClassInfoMap = generateObjectClassInfo(ref);
 		this.factoryPid = factoryPid;
 		this.intercepted = service;
 		
@@ -58,53 +76,77 @@ public class RequestHandlerInterceptor implements RequestHandler {
 	
 	public void removed(ServiceReference<RequestHandler> ref, RequestHandler service) {
 		String factoryPid = (String) ref.getProperty("config.factory-pid");
-		RequestHandler originalHandler = nameConnectors.remove(factoryPid);
-		handlerConnectors.remove(originalHandler);
+		this.intercepted = null;
+		this.factoryPid = null;
+		this.objectClassInfoMap = null;
 		
 		logger.info("Removed Service {}", factoryPid);
 	}
+	
+	private void ensureHasValue() {
+		if (intercepted == null || factoryPid == null) {
+			throw new IllegalStateException("The intercepted instance is missing, cannot proceed");
+		}
+	}
 
+	@Override
 	public Promise<ActionResponse, ResourceException> handleAction(Context context, ActionRequest request) {
-		String factoryPid = handlerConnectors.get(intercepted);
+		ensureHasValue();
 		logger.info("About to execute action for {}", factoryPid);
 		return intercepted.handleAction(context, request);
 	}
 
+	@Override
 	public Promise<ResourceResponse, ResourceException> handleRead(Context context, ReadRequest request) {
-		String factoryPid = handlerConnectors.get(intercepted);
-		logger.info("About to Read for {}", factoryPid);
+		ensureHasValue();
+		logger.info("About to read for {}", factoryPid);
 		Promise<ResourceResponse, ResourceException> handleRead = intercepted.handleRead(context, request);
 		return handleRead;
 	}
 	
 	@Override
 	public Promise<ResourceResponse, ResourceException> handleCreate(Context context, CreateRequest request) {
-		// TODO Auto-generated method stub
+		ensureHasValue();
+		logger.info("About to create for {}", factoryPid);
+		
+		ObjectClassInfoHelper helper = getObjectClassInfoHelper(request.getResourcePathObject());
+		
+		try {
+			Set<Attribute> createAttributes = helper.getCreateAttributes(request, this.cryptoService);
+			logger.info("Create attributes {}", createAttributes);
+		} catch (ResourceException e) {
+			return e.asPromise();
+		}
+		
 		return intercepted.handleCreate(context, request);
 	}
 	
 	@Override
 	public Promise<ResourceResponse, ResourceException> handleDelete(Context context, DeleteRequest request) {
-		// TODO Auto-generated method stub
+		ensureHasValue();
+		logger.info("About to delete for {}", factoryPid);
 		return intercepted.handleDelete(context, request);
 	}
 	
 	@Override
 	public Promise<ResourceResponse, ResourceException> handlePatch(Context context, PatchRequest request) {
-		// TODO Auto-generated method stub
+		ensureHasValue();
+		logger.info("About to Patch for {}", factoryPid);
 		return intercepted.handlePatch(context, request);
 	}
 	
 	@Override
 	public Promise<QueryResponse, ResourceException> handleQuery(Context context, QueryRequest request,
 			QueryResourceHandler handler) {
-		// TODO Auto-generated method stub
+		ensureHasValue();
+		logger.info("About to Query for {}", factoryPid);
 		return intercepted.handleQuery(context, request, handler);
 	}
 	
 	@Override
 	public Promise<ResourceResponse, ResourceException> handleUpdate(Context context, UpdateRequest request) {
-		// TODO Auto-generated method stub
+		ensureHasValue();
+		logger.info("About to Update for {}", factoryPid);
 		return intercepted.handleUpdate(context, request);
 	}
 }
